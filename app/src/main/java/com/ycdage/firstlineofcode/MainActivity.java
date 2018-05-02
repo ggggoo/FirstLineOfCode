642fba2e1b1f6e88de0d8ef22a3bbfa20cfe3af7
yangdage <yc1320011272@gmail.com>
2018-05-02 23:42:02 +0800

    modify mainactivity

--- app/src/main/java/com/ycdage/firstlineofcode/MainActivity.java
+++ app/src/main/java/com/ycdage/firstlineofcode/MainActivity.java
@@ -4,24 +4,44 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
 
-
+    private List<String> mTitleName = new ArrayList<>();
+    private List<Class<?>> mActivities = new ArrayList<>();
+    private ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv = (TextView)findViewById(R.id.sample_text);
-        tv.setOnClickListener(new View.OnClickListener() {
+        addTitle();
+        mListView = findViewById(R.id.main_list);
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(this,android.R.layout.simple_expandable_list_item_1,mTitleName);
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,
-                        NotificationActivity.class));
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                startActivity(new Intent(MainActivity.this,mActivities.get(i)));
             }
         });
+    }
+
+    private void addModule(String titleName, Class<NotificationActivity> activityName) {
+        mTitleName.add(titleName);
+        mActivities.add(activityName);
+    }
+
+
+    private void addTitle() {
+        addModule("Notification",NotificationActivity.class);
     }
 }
