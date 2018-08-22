419b8986618d0db04381b3372db758ddf04f4773
yangchen <1320011272@qq.com>
2018-08-22 14:08:05 +0800

    screen

---
+++ app/src/main/java/com/ycdage/firstlineofcode/screen/ScreenUtilActivity.java
@@ -0,0 +1,13 @@
+package com.ycdage.firstlineofcode.screen;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class ScreenUtilActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
