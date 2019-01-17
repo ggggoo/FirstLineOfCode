6009c25192627c7d97473f50a84454149291c31f
yangdage <yc1320011272@gmail.com>
2019-01-17 17:58:47 +0800

    修改

--- activity/src/main/java/com/ycdage/activity/recyclerview/RecyclerViewActivity.java
+++ activity/src/main/java/com/ycdage/activity/recyclerview/RecyclerViewActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
 import android.view.View;
 
 import com.ycdage.activity.R;
@@ -26,6 +27,7 @@ public class RecyclerViewActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(this.getClass().getSimpleName(),System.currentTimeMillis()+"");
         setContentView(R.layout.activity_recycler_view);
 
         mDatas = new ArrayList<>();
@@ -53,18 +55,30 @@ public class RecyclerViewActivity extends Activity {
         mAdapter.notifyDataSetChanged();
     }
 
+    /**
+     * 设置横排
+     * @param view
+     */
     public void setHorizontal(View view) {
         mLayoutManager = new LinearLayoutManager(this, OrientationHelper.HORIZONTAL, false);
         mRecyclerView.setLayoutManager(mLayoutManager);
         setRecyclerViewDatas();
     }
 
+    /**
+     * 设置竖排
+     * @param view
+     */
     public void setVertical(View view) {
         mLayoutManager = new LinearLayoutManager(this, OrientationHelper.VERTICAL, false);
         mRecyclerView.setLayoutManager(mLayoutManager);
         setRecyclerViewDatas();
     }
 
+    /**
+     * 设置瀑布流
+     * @param view
+     */
     public void setStaggered(View view) {
         mLayoutManager = new StaggeredGridLayoutManager(3, OrientationHelper.VERTICAL);
         mRecyclerView.setLayoutManager(mLayoutManager);
