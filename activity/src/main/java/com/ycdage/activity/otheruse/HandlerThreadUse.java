35cddef40ac64f0b958e1a9b34fa99754f6a39f8
yangchen <1320011272@qq.com>
2020-06-01 15:45:15 +0800

    handlerThread

--- activity/src/main/java/com/ycdage/activity/otheruse/HandlerThreadUse.java
+++ activity/src/main/java/com/ycdage/activity/otheruse/HandlerThreadUse.java
@@ -1,14 +1,21 @@
 package com.ycdage.activity.otheruse;
 
+import android.content.Context;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Message;
+import android.widget.Toast;
 
 public class HandlerThreadUse extends BaseHelper{
 
     private final Handler handler;
+    private Context mContext;
+    int count = 0;
+    private Toast toast;
 
-    public HandlerThreadUse() {
+    public HandlerThreadUse(Context context) {
+        this.mContext = context;
+        this.toast = Toast.makeText(mContext,"这是第"+count+"个",Toast.LENGTH_LONG);
         HandlerThread handlerThread = new HandlerThread("mm"){
             @Override
             public void run() {
@@ -31,6 +38,12 @@ public class HandlerThreadUse extends BaseHelper{
         Message message = new Message();
         message.obj = "发送消息111";
         handler.sendMessage(message);
+    }
+
+    public void clickToast(){
+        toast.setText("这是第"+count+"个");
+        toast.show();
+        count++;
     }
 
 
--- activity/src/main/java/com/ycdage/activity/otheruse/OtherUserActivity.java
+++ activity/src/main/java/com/ycdage/activity/otheruse/OtherUserActivity.java
@@ -1,6 +1,12 @@
 package com.ycdage.activity.otheruse;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
 import com.ycdage.activity.base.BaseFlowActivity;
+import com.ycdage.activity.screen.ScreenUtilActivity;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
@@ -9,8 +15,8 @@ import org.greenrobot.eventbus.ThreadMode;
 public class OtherUserActivity extends BaseFlowActivity {
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         EventBus.getDefault().register(this);
     }
 
@@ -21,8 +27,33 @@ public class OtherUserActivity extends BaseFlowActivity {
         createNewItemTextView(ClassUseHelper.class,"class.forName","useClassForName");
         createNewItemTextView(ClassUseHelper.class,"class.getClass","useGetClass");
 
-        HandlerThreadUse handlerThreadUse = new HandlerThreadUse();
+        HandlerThreadUse handlerThreadUse = new HandlerThreadUse(getBaseContext());
         createNewItemTextView(handlerThreadUse,"handlerThread","sendMsg");
+        createNewItemTextView(handlerThreadUse,"toast","clickToast");
+
+        createNewItemTextView("延迟跳转", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(new Intent(OtherUserActivity.this,ScreenUtilActivity.class));
+                    }
+                },1000);
+            }
+        });
+
+        createNewItemTextView("延迟跳转2", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(new Intent(OtherUserActivity.this,ScreenUtilActivity.class));
+                    }
+                },8000);
+            }
+        });
 
     }
 
@@ -35,5 +66,10 @@ public class OtherUserActivity extends BaseFlowActivity {
     protected void onDestroy() {
         super.onDestroy();
         EventBus.getDefault().unregister(this);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
     }
 }
